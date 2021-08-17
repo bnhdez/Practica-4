748aedf20890b4e09766cf7b5e58ffe5b253668e
Tecnolab <borisnandez93@gmail.com>
2021-08-17 07:34:14 -0600

    cambios

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, prod = 1;
+
+            Console.WriteLine("Ingrese un numero del 1 al 10");
+            num = int.Parse(Console.ReadLine());
+
+            for (int n=1; n<=10; n++)
+            {
+                prod = num * n;
+
+                Console.WriteLine("La multiplicacion es " + num + "x" + n + "=" + prod);
+            }
+        }
+    }
+}
