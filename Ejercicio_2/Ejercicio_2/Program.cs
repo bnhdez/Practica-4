bd25768fbd9792ef51570b7f9afb5329f8bac89d
Tecnolab <borisnandez93@gmail.com>
2021-08-17 07:43:32 -0600

    cambios

---
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant, num;
+            
+            Console.WriteLine("Ingrese la cantidad de numeros a comparar: ");
+            cant = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<=cant; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                num = int.Parse(Console.ReadLine());
+
+
+            }
+
+        }
+    }
+}
