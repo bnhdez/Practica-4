57d98931a538fac1e306a1f433d91aea9a5906c8
Tecnolab <borisnandez93@gmail.com>
2021-08-17 08:01:12 -0600

    cambios

---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int par = 0, impar = 0;
+
+            for (int i=1; i<=25; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("par"+ i);
+                    par++;
+                }
+                else if (i % 2 != 0)
+                {
+                    Console.Write("impar"+ i);
+                    impar++;
+                }
+            }
+        }
+    }
+}
