199d57917d99ae2cc483befe31c6c012de686a64
Tecnolab <borisnandez93@gmail.com>
2021-08-17 08:32:52 -0600

    cambios

--- Ejercicio_4/Ejercicio_4/Program.cs
+++ Ejercicio_4/Ejercicio_4/Program.cs
@@ -11,13 +11,16 @@ namespace Ejercicio_4
             Console.Write("Ingrese la altura del triangulo: ");
             alto = int.Parse(Console.ReadLine());
             int ancho = alto;
+            //Primera funcion for para altura
             for (int a = 1; a <= alto; a++)
             {
-
+                //Segunda funcion for para ancho
                 for (int b = 1; b <= ancho; b++)
                 {
+                    //al usar solo console.write el * se escribira n veces en la misma linea
                     Console.Write("*");
                 }
+                //Al usar console.writeline hara un salto n veces para imprimir el *
                 Console.WriteLine();
                 ancho = ancho - 1;
             }
