97fa1e30f20fb1f4ea7f554ee8614c6412bb9b80
Tecnolab <borisnandez93@gmail.com>
2021-08-17 08:27:28 -0600

    cambios

---
+++ Ejercicio_4/Ejercicio_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alto;
+
+            Console.Write("Ingrese la altura del triangulo: ");
+            alto = int.Parse(Console.ReadLine());
+            int ancho = alto;
+            for (int a = 1; a <= alto; a++)
+            {
+
+                for (int b = 1; b <= ancho; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                ancho = ancho - 1;
+            }
+        }
+    }
+}
