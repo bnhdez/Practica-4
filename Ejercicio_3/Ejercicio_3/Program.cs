3d518403fb82818aabf67a5862c09826e5aa305f
Tecnolab <borisnandez93@gmail.com>
2021-08-17 08:34:13 -0600

    cambios

--- Ejercicio_3/Ejercicio_3/Program.cs
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -8,6 +8,7 @@ namespace Ejercicio_3
         {
             for (int i=1; i<=300; i++)
             {
+                //si el numero se divide entre 2 y tiene residuo 0 es par, si no es impar
                 if (i % 2 == 0)
                 {
                     Console.WriteLine(i + " par");
